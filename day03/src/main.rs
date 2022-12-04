f1ad5fc9faeb3083fde0056f20ec87445f8718ea
Guillaume Vagner <guillaume.vagner@gmail.com>
2022-12-05 00:34:58 +0100

    ⭐⭐ Day 03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -12,10 +12,18 @@ fn main() -> std::io::Result<()> {
 
     let mut total_priority = 0;
 
-    for line in buf_reader.lines() {
-        let bag = line?;
-        let (first_part, second_part) = split_bag(&bag);
-        let duplicate = find_duplicate(first_part, second_part);
+    let mut line_iterator = buf_reader.lines().into_iter();
+
+    while let (Some(first_line), Some(second_line), Some(third_line)) = (
+        line_iterator.next(),
+        line_iterator.next(),
+        line_iterator.next(),
+    ) {
+        let first_elf = first_line?;
+        let second_elf = second_line?;
+        let third_elf = third_line?;
+
+        let duplicate = find_duplicate(&first_elf, &second_elf, &third_elf);
         total_priority += get_letter_score(duplicate);
     }
 
@@ -23,17 +31,9 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn split_bag(bag: &str) -> (&str, &str) {
-    let bag_size = bag.len();
-    let first_part = &bag[..(bag_size / 2)];
-    let second_part = &bag[(bag_size / 2)..];
-
-    (first_part, second_part)
-}
-
-fn find_duplicate(first_part: &str, second_part: &str) -> char {
+fn find_duplicate(first_part: &str, second_part: &str, third_part: &str) -> char {
     for element in first_part.chars().into_iter() {
-        if second_part.contains(element) {
+        if second_part.contains(element) && third_part.contains(element) {
             return element;
         }
     }
