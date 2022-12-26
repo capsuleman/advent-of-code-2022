dd7c7991fcfa9b17d7f31757f57aaaa3e82aa2e1
Guillaume Vagner <guillaume.vagner@gmail.com>
2022-12-26 22:04:36 +0100

    ⭐ Day 07

---
+++ day07/src/main.rs
@@ -0,0 +1,93 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = args.get(1).expect("No input file given.");
+
+    let cd_regex = Regex::new(r"^\$ cd ([a-z]+)$").unwrap();
+    let node_regex = Regex::new(r"^(\d+) (.+)$").unwrap();
+    let dir_regex = Regex::new(r"^dir (.+)$").unwrap();
+
+    let mut current_path = String::from("");
+
+    let mut directory_size: HashMap<String, usize> = HashMap::new();
+
+    let file = File::open(file_path).expect("File not found!");
+    let buf_reader = BufReader::new(file);
+
+    let mut line_iterator = buf_reader.lines().into_iter();
+
+    while let Some(Ok(line)) = line_iterator.next() {
+        if line == "$ cd /" {
+            current_path.push('/');
+            directory_size.insert(String::from("/"), 0);
+            continue;
+        }
+
+        if line == "$ cd .." {
+            current_path.pop();
+            while let Some(char) = current_path.pop() {
+                if char == '/' {
+                    break;
+                }
+            }
+            current_path.push('/');
+            continue;
+        }
+
+        if cd_regex.is_match(&line) {
+            let cd_captures = cd_regex.captures(&line).expect("Issue capturing cd regexp");
+            let directory_name = String::from(&cd_captures[1]);
+
+            current_path.push_str(&directory_name);
+            current_path.push('/');
+            continue;
+        }
+
+        if dir_regex.is_match(&line) {
+            let dir_captures = dir_regex
+                .captures(&line)
+                .expect("Issue capturing dir regexp");
+            let directory_name = String::from(&dir_captures[1]);
+
+            let directory_path = format!("{current_path}{directory_name}/");
+            directory_size.insert(directory_path, 0);
+            continue;
+        }
+
+        if node_regex.is_match(&line) {
+            let node_captures = node_regex
+                .captures(&line)
+                .expect("Issue capturing node regexp");
+
+            let size = node_captures[1]
+                .parse::<usize>()
+                .expect("Can not parse node size");
+
+            let mut new_directory_path = HashMap::new();
+            for (directory_path, directory_size) in directory_size {
+                let mut new_size = directory_size;
+                if current_path.starts_with(&directory_path) {
+                    new_size += size;
+                }
+                new_directory_path.insert(directory_path, new_size);
+            }
+            directory_size = new_directory_path;
+            continue;
+        }
+    }
+
+    let mut accumulated_small_size = 0;
+
+    for &size in directory_size.values() {
+        if size <= 100000 {
+            accumulated_small_size += size;
+        }
+    }
+    println!("Response: {accumulated_small_size}");
+}
