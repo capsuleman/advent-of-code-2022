f2aecb97f153642c0a8dc632c1c2f412a4b40d35
Guillaume Vagner <guillaume.vagner@gmail.com>
2022-12-30 19:04:59 +0100

    ⭐⭐ Day 15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,9 +1,12 @@
 use regex::Regex;
-use std::collections::HashSet;
+use std::cmp::min;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use tqdm::tqdm;
+
+const TUNING_FREQUENCY: u128 = 4000000;
 
 #[derive(Debug)]
 struct Position {
@@ -11,10 +14,10 @@ struct Position {
     y: i32,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 struct Interval {
-    start: i32,
-    end: i32,
+    start: u32,
+    end: u32,
 }
 
 impl Interval {
@@ -47,7 +50,7 @@ impl Interval {
         exclusive_union_vec
     }
 
-    pub fn len(&self) -> i32 {
+    pub fn len(&self) -> u32 {
         self.end - self.start + 1
     }
 }
@@ -55,7 +58,11 @@ impl Interval {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = args.get(1).expect("No input file given.");
-    let depth_search: i32 = args.get(2).expect("No deep given.").parse::<i32>().unwrap();
+    let max_coordinates: u32 = args
+        .get(2)
+        .expect("No max coordinates given.")
+        .parse::<u32>()
+        .unwrap();
 
     let file = File::open(file_path).expect("File not found!");
     let buf_reader = BufReader::new(file);
@@ -67,10 +74,11 @@ fn main() {
     )
     .unwrap();
 
-    let mut impossible_beacon_intervals: Vec<Interval> = Vec::new();
-    let mut beacon_at_depth_search: HashSet<i32> = HashSet::new();
+    let mut impossible_beacon_intervals_list: Vec<Vec<Interval>> =
+        (0..max_coordinates).map(|_| Vec::new()).collect();
 
     while let Some(Ok(line)) = line_iterator.next() {
+        println!("{line}");
         let regex_captures = parser_regex
             .captures(&line)
             .expect("Issue capturing input.");
@@ -84,45 +92,65 @@ fn main() {
             y: regex_captures[4].parse::<i32>().unwrap(),
         };
 
-        if beacon_position.y == depth_search {
-            beacon_at_depth_search.insert(beacon_position.x);
-        }
+        for (depth_search, impossible_beacon_intervals) in
+            tqdm(impossible_beacon_intervals_list.iter_mut().enumerate())
+        {
+            let distance_sensor_beacon = get_manhattan_distance(&sensor_position, &beacon_position);
+            let distance_sensor_depth = (depth_search as i32).abs_diff(sensor_position.y);
 
-        let distance_sensor_beacon = get_manhattan_distance(&sensor_position, &beacon_position);
-        let distance_sensor_depth = depth_search.abs_diff(sensor_position.y);
-
-        if distance_sensor_depth > distance_sensor_beacon {
-            continue;
-        }
-
-        let diff_distances = (distance_sensor_beacon - distance_sensor_depth) as i32;
-
-        let mut intervals_to_insert = Vec::from([Interval {
-            start: sensor_position.x - diff_distances,
-            end: sensor_position.x + diff_distances,
-        }]);
-
-        for impossible_beacon_interval in impossible_beacon_intervals.iter() {
-            let mut new_intervals_to_insert: Vec<Interval> = Vec::new();
-            for interval_to_insert in intervals_to_insert.into_iter() {
-                let exclusive_union =
-                    impossible_beacon_interval.exclusive_union(&interval_to_insert);
-
-                new_intervals_to_insert.extend(exclusive_union);
+            if distance_sensor_depth > distance_sensor_beacon {
+                continue;
             }
-            intervals_to_insert = new_intervals_to_insert;
-        }
 
-        impossible_beacon_intervals.extend(intervals_to_insert);
+            let diff_distances = (distance_sensor_beacon - distance_sensor_depth) as i32;
+
+            let start = u32::try_from(sensor_position.x - diff_distances).unwrap_or(0);
+            let end = min(max_coordinates, (sensor_position.x + diff_distances) as u32);
+
+            if start > max_coordinates {
+                continue;
+            }
+
+            let mut intervals_to_insert = Vec::from([Interval { start, end }]);
+
+            for impossible_beacon_interval in impossible_beacon_intervals.iter() {
+                let mut new_intervals_to_insert: Vec<Interval> = Vec::new();
+                for interval_to_insert in intervals_to_insert.into_iter() {
+                    let exclusive_union =
+                        impossible_beacon_interval.exclusive_union(&interval_to_insert);
+
+                    new_intervals_to_insert.extend(exclusive_union);
+                }
+                intervals_to_insert = new_intervals_to_insert;
+            }
+
+            impossible_beacon_intervals.extend(intervals_to_insert);
+        }
     }
 
-    let impossible_beacon_count = impossible_beacon_intervals
+    impossible_beacon_intervals_list
         .iter()
-        .map(|interval| interval.len())
-        .sum::<i32>() as usize
-        - beacon_at_depth_search.len();
+        .enumerate()
+        .for_each(|(y, impossible_beacon_intervals)| {
+            let impossible_beacon_count = impossible_beacon_intervals
+                .iter()
+                .map(|interval| interval.len())
+                .sum::<u32>();
 
-    println!("{impossible_beacon_count}")
+            if impossible_beacon_count < max_coordinates + 1 {
+                for x in 0..max_coordinates + 1 {
+                    let contains_x = impossible_beacon_intervals
+                        .iter()
+                        .any(|interval| interval.start <= x && x <= interval.end);
+                    if !contains_x {
+                        println!(
+                            "Missing beacon at {x}, {y} -> {}",
+                            x as u128 * TUNING_FREQUENCY + y as u128
+                        );
+                    }
+                }
+            }
+        });
 }
 
 fn get_manhattan_distance(position_a: &Position, position_b: &Position) -> u32 {
